de371e0b37a479aef63ce8f98f51a6cc8b1a901a
FajarRisk1 <ajabagus833@gmail.com>
2023-04-30 21:22:07 +0800

    Penambahan

---
+++ customer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header class="header" data-header>
+        <div class="container">
+            <nav class="navbar" data-navbar>
+                <li class="navbar-item">
+                    <a href="#" class="label-lg navbar-link has-after active">Home</a>
+                </li>
+                <li class="navbar-item">
+                    <a href=" " class="label-lg navbar-link has-after">A</a>
+                </li>
+                <li class="navbar-item">
+                    <a href=" " class="label-lg navbar-link has-after">About</a>
+                </li>
+                <li class="navbar-item">
+                    <a href=" " class="label-lg navbar-link has-after">Profile</a>
+                </li>  										
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
