e137f4a1d7c597e797923de789ff1f7e971adea1
Raihan Daiva Geralda <daivageralda831@gmail.com>
2023-05-14 18:37:37 +0800

    new features

--- pa_web(fokus)/customer_tiket.php
+++ pa_web(fokus)/customer_tiket.php
@@ -40,12 +40,11 @@ $result = tampil_data()
             <p class="card-text"><?php echo $deskripsi ?></p>
             <p class="card-text">Rp <?php echo number_format($harga, 0, ',', '.') ?></p>
             <button type="button" class="btn btn-primary btn-detail" data-toggle="modal" data-target="#detailModal-<?php echo $id ?>">Lihat Detail</button>
-            <button type="button" class="btn btn-custom" data-toggle="modal" data-target="#pesanModal-<?php echo $id ?>">Pesan Tiket</button>
+            <button type="button" class="btn btn-custom" onclick="hitungHarga(<?php echo $id ?>)" data-toggle="modal" data-id="<?php echo $id; ?>" data-target="#pesanModal-<?php echo $id; ?>" data-harga="<?php echo $harga; ?>">Pesan Tiket</button>
           </div>
         </div>
         <!-- tambahkan modal disini -->
         <div class="modal fade" id="detailModal-<?php echo $id ?>" tabindex="-1" role="dialog" aria-labelledby="detailModalLabel-<?php echo $id ?>" aria-hidden="true">
-
           <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: none; max-height: none; width: 40%; height: 500px; ">
             <div class="modal-content">
               <div class="modal-header">
@@ -76,64 +75,46 @@ $result = tampil_data()
           </div>
         </div>
         <div class="modal fade" id="pesanModal-<?php echo $id ?>" tabindex="-1" role="dialog" aria-labelledby="pesanModalLabel-<?php echo $id ?>" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: none; max-height: none; width: 40%; height: 500px; ">
+          <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="pesanModalLabel-<?php echo $id ?>"><?php echo $nama_paket ?></h5>
+                <h5 class="modal-title" id="pesanModalLabel-<?php echo $id ?>">Pesan Tiket</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-                <div class="modal-body">
-                  <form method="post" action="customer_controller.php" enctype="multipart/form-data">
-                    <input type="hidden" name="id_paket" value="<?php echo $id; ?>">
-                    <div class="form-row">
-                      <div class="col-md-6 mb-3">
-                        <label for="tanggal_berangkat">Tanggal Berangkat:</label>
-                        <input type="date" class="form-control" id="tanggal_berangkat" name="tanggal_berangkat" onchange="setTanggalKembali()" required>
-                      </div>
-                    <div class="col-md-6 mb-3">
-                      <label for="tanggal_kembali">Tanggal Kembali:</label>
-                      <input type="date" class="form-control" id="tanggal_kembali" name="tanggal_kembali" readonly required>
-                    </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="col-md-6 mb-3">
-                        <label for="jumlah_orang">Jumlah Orang:</label>
-                        <input type="number" class="form-control" id="jumlah_orang" name="jumlah_orang" min="1" onchange="setTotalHarga()" required>
-                      </div>
-                      <div class="col-md-6 mb-3">
-                        <label for="kebutuhan_khusus">Kebutuhan Khusus:</label>
-                        <input type="text" class="form-control" id="kebutuhan_khusus" name="kebutuhan_khusus">
-                      </div>
-                    </div>
-                    <div class="form-row">
-                      <div class="col-md-6 mb-3">
-                        <label for="harga_paket">Harga Paket:</label>
-                        <input type="hidden" class="form-control" id="harga_paket" name="harga_paket" value="<?php echo $harga; ?>" readonly>
-                      </div>
-                      <div class="col-md-6 mb-3">
-                        <label for="total_harga">Total Harga:</label>
-                        <input type="text" class="form-control" id="total_harga" name="total_harga" readonly>
-                      </div>
-                    </div>
-                    <div class="form-group text-center">
-                      <button type="submit" class="btn btn-primary float-right" name="tambah">Tambah</button>
-                    </div>
-                  </form>
-                </div>
-
+              <div class="modal-body">
+                <form method="post" action="customer_controller.php">
+                  <div class="form-group">
+                    <label for="jumlah_orang-<?php echo $id ?>">Jumlah Orang</label>
+                    <input type="number" class="form-control" id="jumlah_orang-<?php echo $id ?>" min="1" max="6" step="1" value="1" oninput="hitungHarga(<?php echo $id ?>)">
+                  </div>
+                  <div class="form-group">
+                    <label for="total_harga-<?php echo $id ?>">Total Harga</label>
+                    <input type="text" class="form-control" id="total_harga-<?php echo $id ?>" value="<?php echo $harga ?>" disabled>
+                    <input type="hidden" class="form-control" id="total_hargaHid-<?php echo $id ?>" value="<?php echo $harga ?>" disabled>
+                    <div class="tampungHarga<?php echo $id ?>" hidden><?php echo $harga ?></div>
+                  </div>
+                  <div class="form-group">
+                    <label for="tanggal_berangkat-<?php echo $id ?>">Tanggal Berangkat</label>
+                    <input type="date" class="form-control" id="tanggal_berangkat-<?php echo $id ?>" oninput="hitungTanggal(<?php echo $id ?>)">
+                  </div>
+                  <div class="form-group">
+                    <label for="tanggal_kembali-<?php echo $id ?>">Tanggal Kembali</label>
+                    <input type="date" class="form-control" id="tanggal_kembali-<?php echo $id ?>" disabled>
+                    <div class="tampungDurasi<?php echo $id ?>" hidden><?php echo $durasiAngka ?></div>
+                  </div>
+                </form>
+              </div>
               <div class="modal-footer">
-                <button type="button" class="btn btn-secondary mx-auto d-block" data-dismiss="modal">Tutup</button>
-
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
               </div>
             </div>
           </div>
         </div>
-        <?php endforeach;?>
-      </div>
+      <?php endforeach;?>
+    </div>
   </div>
-
 </body>
 <script>
 $(document).ready(function() {
@@ -158,39 +139,48 @@ $(document).ready(function() {
     $('.btn-custom').removeClass('active');
   });
 });
-function formatNumber(num) {
-  var str = num.value.replace(/[^\d]/g, '');
-  while (/(\d+)(\d{3})/.test(str)) {
-    str = str.replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
-  }
-  num.value = str;
-}
-
-function unFormatNumber(num) {
-  num.value = num.value.replace(/,/g, '');
-}
-
-let durasiPaket = 0;
-
-function setDurasiPaket(durasi) {
-  durasiPaket = durasi;
-}
-
-function setTanggalKembali() {
-  const tanggalBerangkat = new Date(document.getElementById("tanggal_berangkat").value);
-  tanggalBerangkat.setDate(tanggalBerangkat.getDate() + durasiPaket);
-  const tanggalKembali = tanggalBerangkat.toISOString().slice(0, 10);
-  document.getElementById("tanggal_kembali").value = tanggalKembali;
-}
-
-function hitungTotalHarga() {
-    const hargaPaket = parseInt(document.getElementById("harga_paket").value);
-    const jumlahOrang = parseInt(document.getElementById("jumlah_orang").value);
-    const totalHarga = hargaPaket * jumlahOrang;
-    document.getElementById("total_harga").value = totalHarga;
-  }
-
-  document.getElementById("jumlah_orang").addEventListener("input", hitungTotalHarga);
-  document.getElementById("jumlah_orang").addEventListener("change", hitungTotalHarga);
 </script>
+<script>
+function formatRupiah(angka) {
+  let numberString = angka.toString().replace(/[^,\d]/g, '');
+  let split = numberString.split(',');
+  let sisa = split[0].length % 3;
+  let rupiah = split[0].substr(0, sisa);
+  let ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+  if (ribuan) {
+    let separator = sisa ? '.' : '';
+    rupiah += separator + ribuan.join('.');
+  }
+
+  rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+  return 'Rp. ' + rupiah;
+}
+
+function hitungHarga(id) {
+  let jumlahOrang = document.getElementById('jumlah_orang-' + id).value;
+  let hargaView = document.querySelector('.tampungHarga' + id).innerHTML;
+  let hargaHid = document.querySelector('.tampungHarga' + id).innerHTML;
+  let totalHargaView = jumlahOrang * hargaView;
+  let totalHargaHid = jumlahOrang * hargaView;
+  document.getElementById('total_harga-' + id).value = formatRupiah(totalHargaView);
+  document.getElementById('total_hargaHid-' + id).value = formatRupiah(totalHargaView);
+}
+function hitungTanggal(id) {
+  let tanggalBerangkat = new Date(document.getElementById('tanggal_berangkat-' + id).value);
+  let tanggalArray = tanggalBerangkat.toLocaleDateString("en-US").split("/");
+  let tanggal = parseInt(tanggalArray[1]);
+  let bulan = parseInt(tanggalArray[0]) - 1;
+  let tahun = parseInt(tanggalArray[2]);
+  let durasi = parseInt(document.querySelector('.tampungDurasi' + id).innerHTML);
+  tanggalBerangkat.setDate(tanggal + durasi);
+  tanggalBerangkat.setFullYear(tahun, bulan, tanggal + durasi);
+  document.getElementById('tanggal_kembali-' + id).value = tanggalBerangkat.toISOString().split('T')[0];
+}
+
+
+</script>
+
+
+
 </html>
