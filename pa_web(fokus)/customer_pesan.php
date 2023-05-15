da9ad75f0c1155c5d4abd236ae0631853cd4813b
FajarRisk1 <ajabagus833@gmail.com>
2023-05-15 19:50:40 +0800

    Minor Update

--- pa_web(fokus)/customer_pesan.php
+++ pa_web(fokus)/customer_pesan.php
@@ -11,16 +11,41 @@ $result = tampil_data();
 <head>
 	<title>Card Paket Travel</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
 	<link rel="stylesheet" type="text/css" href="css/customer.css">
 
 </head>
 <body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Travel5</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="customer_tiket.php">Tiket Saya</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="customer_controller.php?action=logout">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   <!-- untuk memeriksa apakah ada pesan yang akan ditampilkan atau tidak -->
   <?php
   if (isset($_GET['pesan'])) {
@@ -37,13 +62,6 @@ $result = tampil_data();
           </script>";
   }
   ?>
-
-    <div class="d-flex justify-content-end">
-      <a class="btn btn-primary ml-auto" href="customer_tiket.php">Tiket Saya</a>
-      <a class="btn btn-primary ml-2" href="customer_controller.php?action=logout">Logout</a>
-    </div>
-
-
   <!-- tampilan utama pada halaman ini  -->
   <div class="container" style="margin-top: 100px;">
     <div class="card-container">
